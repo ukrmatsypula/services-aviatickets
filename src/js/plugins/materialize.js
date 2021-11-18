3c13ab2dc7c4f07e0912a7cff9e02b8b209df4a5
Roman Matsypula <ukrmatsypula@gmail.com>
2021-11-18 15:00:26 +0200

    changes

--- src/js/plugins/materialize.js
+++ src/js/plugins/materialize.js
@@ -1,2 +1,38 @@
 import "materialize-css/dist/css/materialize.min.css";
 import "materialize-css/dist/js/materialize.min.js";
+
+// Select
+const select = document.querySelectorAll("select");
+M.FormSelect.init(select, {});
+
+export function getSelectInstance(elem) {
+  return M.FormSelect.getInstance(elem);
+}
+
+// Autocomplete
+const autocomplete = document.querySelectorAll(".autocomplete");
+M.Autocomplete.init(autocomplete, {
+  data: {
+    Apple: null,
+    Microsoft: null,
+    Google: "https://placehold.it/250x250",
+  },
+});
+
+export function getAutocompleteInstance(elem) {
+  return M.Autocomplete.getInstance(elem);
+}
+
+getAutocompleteInstance(autocomplete);
+
+// datepicker
+const datepickers = document.querySelectorAll(".datepicker");
+M.Datepicker.init(datepickers, {
+  showClearBtn: true,
+});
+
+export function getDatePickerInstance(elem) {
+  return M.Datepicker.getInstance(elem);
+}
+
+getDatePickerInstance(datepickers);
