d847e9556fb7ec7033f791c3c6af4f8b15a497b6
Roman Matsypula <ukrmatsypula@gmail.com>
2021-11-23 22:47:33 +0200

    serialize tickets

--- src/js/store/locations.js
+++ src/js/store/locations.js
@@ -21,7 +21,7 @@ class Locations {
     this.cities = this.serializeCities(cities);
     this.shortCitiesList = this.createShortCitiesList(this.cities);
     this.airlines = this.serializeAirlines(airlines);
-    console.log(this.airlines);
+
     return response;
   }
 
@@ -45,12 +45,20 @@ class Locations {
   serializeCities(cities) {
     // { 'City name, Country name }
     return cities.reduce((acc, city) => {
-      const country_name = this.getCountryNameByCode(city.country_code);
-      const city_name = city.name || city.name_translations.en;
-      const key = `${city_name}, ${country_name}`;
-      acc[key] = city;
+      const country_name = this.countries[city.country_code].name;
+      city.name = city.name || city.name_translations.en;
+      const full_name = `${city.name}, ${country_name}`;
+      acc[city.code] = {
+        ...city,
+        country_name,
+        full_name,
+      };
       return acc;
     }, {});
+  }
+
+  getCityNameByCode(code) {
+    return this.cities[code].name;
   }
 
   getAirlineNameByCode(code) {
@@ -62,12 +70,16 @@ class Locations {
   }
 
   getCityCodeByKey(key) {
-    return this.cities[key].code;
+    const city = Object.values(this.cities).find(
+      (item) => item.full_name === key
+    );
+
+    return city.code;
   }
 
   createShortCitiesList(cities) {
-    return Object.entries(cities).reduce((acc, [key]) => {
-      acc[key] = null;
+    return Object.entries(cities).reduce((acc, [, city]) => {
+      acc[city.full_name] = null;
       return acc;
     }, {});
   }
@@ -79,7 +91,20 @@ class Locations {
 
   async fetchTickets(params) {
     const response = await this.api.prices(params);
-    console.log(response);
+    this.lastSearch = this.serializeTickets(response.data);
+    console.log(this.lastSearch);
+  }
+
+  serializeTickets(tickets) {
+    return Object.values(tickets).map((ticket) => {
+      return {
+        ...ticket,
+        origin_name: this.getCityNameByCode(ticket.origin),
+        destination_name: this.getCityNameByCode(ticket.destination),
+        airline_logo: this.getAirlineLogoByCode(ticket.airline),
+        airline_name: this.getAirlineNameByCode(ticket.airline),
+      };
+    });
   }
 }
 
